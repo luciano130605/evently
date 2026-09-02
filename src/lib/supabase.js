f2779150a8ba57879d2e5b49f0173f3e07bc2963
luciano <luciano.delapena@gmail.com>
2026-09-01 23:52:52 -0300

    fix vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://tmpwfusuzrmycmwuuotk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcHdmdXN1enJteWNtd3V1b3RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMjIyMTQsImV4cCI6MjEwMzc5ODIxNH0.PxZnMKoHYZ9BELOGUqQGBPmd4sLNw0qSZHg8NfHVzC0';
 const isTest = import.meta.env.MODE === "test";
 
 export const supabase =
